e5c8d0aaf87902f4ece8eda38d297cb7f77fa8b4
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2022-06-10 16:13:07 +0530

    Provided input through echo and pipe
    * instead of using hereString

--- test/testFillForm.sh
+++ test/testFillForm.sh
@@ -3,15 +3,15 @@
 INPUT="Prince\n2001-07-19\ndrawing,sketching\n9876543210\nDixi\nNashik"
 EXPECTED='{"name":"Prince","dob":{"year":2001,"month":7,"day":19},"hobbies":["drawing","sketching"],"mobileNo":"9876543210","address":"Dixi\nNashik"}'
 
-node "./fillForm.js" <<< "${INPUT}" &> /dev/null
+echo -e "${INPUT}" | node "./fillForm.js" &> /dev/null
 echo -n "${EXPECTED}" > "./test/expectedForm.json"
-diff "./form.json" "./test/expectedForm.json" > /dev/null
+diff "./form.json" "./test/expectedForm.json"
 
 EXITCODE=$?
-STATUS="FAIL"
+STATUS="❌FAIL"
 if [[ ${EXITCODE} -eq 0 ]]
 then
-  STATUS="PASS"
+  STATUS="✅PASS"
 fi
 
 echo -e "fillForm\n\v${STATUS}"
